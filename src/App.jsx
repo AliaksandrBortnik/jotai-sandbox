581394b0bfcedffe347a2dfe4875e51f09889482
Alex Bortnik <bortnik.ar@gmail.com>
2025-08-17 22:09:08 +0200

    Added a few atoms: base, derived and sync with StorageAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {atom, useAtom, useAtomValue} from 'jotai';
+import {atomWithStorage} from 'jotai/utils';
+
+const counter = atom(0);
+// Sync with LocaleStorage/SessionStorage.
+const darkTheme = atomWithStorage('darkTheme', false);
+// Derived atom.
+const friendsStatus = atom([
+    {name: "John", online: true},
+    {name: "David", online: false},
+    {name: "Micheal", online: true}
+]);
+const onlineFriends = atom(get => get(friendsStatus).filter(f => f.online));
+
+const name = atom('');
+const headerTitle = atom(get => get(name).toUpperCase());
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isDarkTheme, switchTheme] = useAtom(darkTheme);
+    const [count, setCount] = useAtom(counter);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // Read-only atom.
+    const friends = useAtomValue(onlineFriends);
+
+    const [personName, setPersonName] = useAtom(name);
+    const title = useAtomValue(headerTitle);
+
+    return (
+        <>
+            <button style={{backgroundColor: isDarkTheme ? '#aaa' : '#fff'}}
+                    onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <button onClick={() => switchTheme(prev => !prev)}>
+                Switch Theme
+            </button>
+            <ul>
+                {friends.map(f => <li key={f.name}>{f.name}</li>)}
+            </ul>
+
+            <input type="text" placeholder="Enter your name" value={personName}
+                   onChange={(e) => setPersonName(e.target.value)}/>
+            <h3>{title}</h3>
+        </>
+    )
 }
 
 export default App
