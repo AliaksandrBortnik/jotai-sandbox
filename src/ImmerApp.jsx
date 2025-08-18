d39d3bdd5eac78ca8420cf574ba16cdc9359a1bc
Alex Bortnik <bortnik.ar@gmail.com>
2025-08-18 23:02:01 +0200

    Jotai + immer

---
+++ src/ImmerApp.jsx
@@ -0,0 +1,31 @@
+import { useAtom } from 'jotai';
+import { atomWithImmer } from 'jotai-immer';
+
+const todo = {
+    todo: {
+        person: {
+            name: "David",
+            title: {
+                goal: "old todo"
+            },
+        }
+    }
+};
+const immerAtom = atomWithImmer(todo);
+
+export default function ImmerApp() {
+    const [todoAtom, setAtomTodo] = useAtom(immerAtom);
+    const updateTodo = () => {
+        setAtomTodo(state => {
+            state.todo.person.title.goal = "new title";
+            return state;
+        });
+    }
+    return (
+        <div className="app">
+            <h3>Name: {todoAtom.todo.person.name}</h3>
+            <p>Todo: {todoAtom.todo.person.title.goal}</p>
+            <button onClick={updateTodo}>Update Todo</button>
+        </div>
+    )
+}
